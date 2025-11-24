4c3ccd1cebebdca8bad8ab41bd41dbbbb575e7cc
Francis Tan <499166435@qq.com>
2025-11-24 06:04:28 +0000

    refactor: Improve problem 0003 with sliding window technique
    Add optimized sliding window implementation while keeping old solution for reference.

--- problems/0003-longest-substring-without-repeating-characters/solution.cpp
+++ problems/0003-longest-substring-without-repeating-characters/solution.cpp
@@ -6,7 +6,40 @@ using namespace std;
 
 class Solution {
 public:
+    // Sliding Window Pattern - 11/24/2025
+    // Ajusted implementation using sliding window technique
     int lengthOfLongestSubstring(string s) {
+        if (s.size() < 2) return s.size();
+
+        unordered_map<char, int> seen;
+        int pl = 0;
+        int pr = 0;
+        int maxLength = 1;
+        int counter = 0;
+        while (pr < s.size()) {
+            char c = s[pr];
+            auto it = seen.find(c);
+            if (it == seen.end() || it->second < pl) {
+                counter++;
+            } else {
+                if (counter > maxLength) {
+                    maxLength = counter;
+                }
+                counter = pr - it->second;
+                pl = it->second + 1;
+            }
+            seen[c] = pr;
+            pr++;
+        }
+
+        if (counter > maxLength) {
+            maxLength = counter;
+        }
+        
+        return maxLength;
+    }
+
+    int lengthOfLongestSubstring_BeforeLearningSlidingWindow_Pattern(string s) {
         if (s.empty()) return 0;
 
         unordered_map<char, int> charIndexMap;
